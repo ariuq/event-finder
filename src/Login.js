6163d20f30303219304caa318a7d8bf704d05e20
ariuq <ariud015@gmail.com>
2023-11-27 15:56:35 +0800

    ll

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from "./assets/logo512.png"
+
+const Login = () => {
+  return (
+    <div>
+      <img src={logo}/>
+    </div>
+  )
+}
+
+export default Login
