d9039bd5fdbbc127523f338c52ce41d5f5d63868
ariuq <ariud015@gmail.com>
2023-11-27 16:06:32 +0800

    ll

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-import Login from './Login';
-=======
-import Events from './Events';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
+
       <Login/>
-=======
-      <Events/>
->>>>>>> Stashed changes
+
     </div>
   );
 }
