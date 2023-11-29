33d640d3450eb473aee5f7bb85cc1029596fee0e
Batkholboo <78951364+batkhoboo@users.noreply.github.com>
2023-11-29 14:37:57 +0800

    k

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 
 import Details from './Details';
+import Events from './Events';
 
 function App() {
   return (
     <div className="App">
       
-      <Details/>
+      <Events/>
 
     </div>
   );
--- src/Events.js
+++ src/Events.js
@@ -3,6 +3,11 @@ import Header from './components/Header'
 import poster from './assets/poster.png'
 import Card from './components/Card'
 import event1 from './assets/event1.png'
+import event2 from './assets/event2.png'
+import event3 from './assets/event3.png'
+import event4 from './assets/event4.png'
+import event5 from './assets/event5.png'
+import event6 from './assets/event6.png'
 import Footer from './components/Footer'
 import './style/Events.css'
 const Events = () => {
@@ -13,10 +18,14 @@ const Events = () => {
       <h1>Education:</h1>
       <div className='Carts1'>
         <Card name='Virginia Hiring Event' image={event1} address='Shangri-La' month='OCT' day='19'/>
-        
-        <Card name='Virginia Hiring Event' image={event1} address='Shangri-La' month='OCT' day='19'/>
-        
-        <Card name='Virginia Hiring Event' image={event1} address='Shangri-La' month='OCT' day='19'/>
+        <Card name='Virginia Hiring Event' image={event2} address='Shangri-La' month='OCT' day='19'/>
+        <Card name='Virginia Hiring Event' image={event3} address='Shangri-La' month='OCT' day='19'/>
+      </div>
+      <h1>Education:</h1>
+      <div className='Carts2'>
+        <Card name='Virginia Hiring Event' image={event4} address='Shangri-La' month='OCT' day='19'/>
+        <Card name='Virginia Hiring Event' image={event5} address='Shangri-La' month='OCT' day='19'/>
+        <Card name='Virginia Hiring Event' image={event6} address='Shangri-La' month='OCT' day='19'/>
       </div>
       <Footer/>
     </div>
