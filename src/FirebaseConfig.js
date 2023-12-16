782fe73b499f885aa8f88125fea79e64ab068824
ariuq <ariud015@gmail.com>
2023-12-16 18:10:44 +0800

    firebase configuration

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-14YPK6Cd4QGmKXrzJdx5UD3rjQuMQTM",
+  authDomain: "event-finder-ecb3a.firebaseapp.com",
+  projectId: "event-finder-ecb3a",
+  storageBucket: "event-finder-ecb3a.appspot.com",
+  messagingSenderId: "384984205653",
+  appId: "1:384984205653:web:04b8ba0547292564e307dc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
